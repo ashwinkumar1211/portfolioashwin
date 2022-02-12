b3d32f63326b7967f4c179b8036f4f537c27d821
Ashwincodez <ashwinkumar.m1211@gmail.com>
2022-02-13 01:22:17 +0530

    initial commit (2)

--- src/portfolio.js
+++ src/portfolio.js
@@ -131,15 +131,15 @@ const techStack = {
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      progressPercentage: "70%" //Insert relative proficiency in percentage
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "80%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "70%"
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
